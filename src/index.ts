29388c551a432b89e59e677ca3c1ff5f3e599fd0
gabriellydsantos <gabydsantos24@gmail.com>
2024-05-07 16:21:31 -0300

    configurado projeto typescript

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+
+
+// // const myName:string = "joao";
+// let year = 1990;
+// let age= year - 2024;
+
+// console.log(`${age}`);
+
+const num1 = 20;
+const num2 = 20;
+
+
+// const num1 = 2;
+// const num2 = "2";
+
+// if(num1 === num2){
+//     console.log("igual")
+// }else {
+
+//     console.log("nao e igual")
+// }
+
+
+// if(mes === 1) {
+//   console.log("Janeiro");
+// }else if(mes === 2) {
+//     console.log("fevereiro");    
+// }
+
+
