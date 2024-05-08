ba9ce6640f04e642b179761898b3dbaf83073c83
gabriellydsantos <gabydsantos24@gmail.com>
2024-05-08 18:40:36 -0300

    Finalizado os exercicios

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,61 @@
 //Exercicio 1
 
 // Declared variable
-const a = 5;
-const b = 3;
-const c = 10;
-
-// Calculate the sum of A and B
-const sum = a + b;
-
-if (sum < c) {
-    console.log(`A soma de A e B é menor que C.`);
-} else {
-    console.log(`A soma de A e B não é menor que C.`);
-}
-
+// const a = 5;
+// const b = 3;
+// const c = 10;
 
 
 //Exercicio 2
+
+// const num1 = 10;
+// const antecessor = num1 - 1;
+// const sucessor = num1 + 1;
+
+// console.log(`O antecessor de ${num1} é ${antecessor}`);
+// console.log(`O sucessor de ${num1} é ${sucessor}`);
+
+
 //Exercicio 3
+// var media
+// const n1 = 10;
+// const n2 = 5;
+// const n3 = 5;
+
+// media = (n1 + n2 + n3) /3
+
+// console.log(`A media desse aluno é ${media}`)
+
 //Exercicio 4
+
+// const nome = "Gaby";
+// const idade = 17;
+
+// if (idade >= 18) {
+//     console.log(`${nome} é maior de idade.`);
+// } else {
+//     console.log(`${nome} é menor de idade.`);
+// }
+    
 //Exercicio 5
 
-
-// const num1 = 2;
-// const num2 = 3; // Define num2 here or assign it a value from elsewhere
-
-// if (num1 === num2) {
-//     console.log("igual");
-// } else {
-//     console.log("não é igual");
+// const num = 2
+// //declarando a varivel com let pois não ira ser alterada
+// for (let i = 0; i <= 20; i++) {
+// //   console.log(num * i)
+//   console.log(`${num} * ${i} = ${num * i}`);
 // }
+
+
+
+//rascunho de examplo
+// // Calculate the sum of A and B
+// const sum = a + b;
+
+// if (sum < c) {
+//     console.log(`A soma de A e B é menor que C.`);
+// } else {
+//     console.log(`A soma de A e B não é menor que C.`);
+// }
+
 
