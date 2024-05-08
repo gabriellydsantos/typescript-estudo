4f4700edf1c7f42a315733810ee41554b14cfda6
gabriellydsantos <gabydsantos24@gmail.com>
2024-05-08 17:29:09 -0300

    Finalização do exercicio 1

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,33 @@
 //Exercicio 1
+
+// Declared variable
+const a = 5;
+const b = 3;
+const c = 10;
+
+// Calculate the sum of A and B
+const sum = a + b;
+
+if (sum < c) {
+    console.log(`A soma de A e B é menor que C.`);
+} else {
+    console.log(`A soma de A e B não é menor que C.`);
+}
+
+
+
 //Exercicio 2
 //Exercicio 3
 //Exercicio 4
 //Exercicio 5
 
 
-
-
-
-
 // const num1 = 2;
+// const num2 = 3; // Define num2 here or assign it a value from elsewhere
 
-// if(num1 === num2){
-//     console.log("igual")
-// }else {
-//     console.log("nao e igual")
+// if (num1 === num2) {
+//     console.log("igual");
+// } else {
+//     console.log("não é igual");
 // }
-
-
 
